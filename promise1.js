2f9ab9b84350b76b53d4ecb0fefe5719bb435e56
MandhadiManjusha <138752399+MandhadiManjusha@users.noreply.github.com>
2023-07-12 13:13:38 +0530

    Add files via upload

---
+++ promise1.js
@@ -0,0 +1,15 @@
+const event = new Promise((resolve, reject) => {
+   var name = "Yamini";
+   if (name == "Sunitha") {
+       resolve(name);
+} else {
+reject("Name was not Sunitha,name was " + name);
+}
+});
+
+event.then((name) => {
+console.log(name);
+})
+event.catch((err) => {
+console.log(err);
+});
